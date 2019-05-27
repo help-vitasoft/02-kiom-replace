79a802ad5f6080040960bffb3069a561e140e656
scy <scy@vitasoft.co.kr>
2019-05-27 14:46:13 +0900

    first

---
+++ spec/simple-replace-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SimpleReplaceView from '../lib/simple-replace-view';
+
+describe('SimpleReplaceView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
